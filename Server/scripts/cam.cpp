439e5862fee77f90d3fec67ad03d77db603e2cdd
wladimiiir <wladimiiir@darkstar.example.net>
2013-06-17 06:47:46 +0200

    critter action manager in C++ ready (needs performance and memory management improvements)

--- Server/scripts/cam.cpp
+++ Server/scripts/cam.cpp
@@ -1,27 +1,50 @@
 #include "cam.h"
 
+void Start(Critter* critter, asIScriptObject* actionObj, bool immediately);
+
+EXPORT void InitLibrary();
+
 // Entry point
 FONLINE_DLL_ENTRY( isCompiler )
 {
     if( isCompiler )
         return;
 
-	Log("Hello");
+	Log("Registering CritterActionManager types...\n");
+	ASEngine->RegisterInterface("Condition");
+	ASEngine->RegisterInterfaceMethod("Condition", "bool Check(Critter&, Critter@, Item@)");
 	
-	ASEngine->RegisterInterface("IAction");
-	ASEngine->RegisterInterfaceMethod("IAction", "IAction@ GetSuperAction()");
-	ASEngine->RegisterInterfaceMethod("IAction", "void SetSuperAction(IAction@ superAction)");
-	ASEngine->RegisterInterfaceMethod("IAction", "void CollectListeningActions(any& collection)");
-	ASEngine->RegisterInterfaceMethod("IAction", "void StartNextAction(Critter& critter)");
-	ASEngine->RegisterInterfaceMethod("IAction", "bool Check(Critter& critter, Critter@ someCritter, Item@ someItem)");
-	ASEngine->RegisterInterfaceMethod("IAction", "bool Start(Critter& critter)");
-	ASEngine->RegisterInterfaceMethod("IAction", "void Cancel(Critter& critter)");
-	ASEngine->RegisterInterfaceMethod("IAction", "string GetInfo()");
-	ASEngine->RegisterInterfaceMethod("IAction", "void Idle(Critter& critter)");
-	ASEngine->RegisterInterfaceMethod("IAction", "bool Attack(Critter& critter, Critter& target)");
+	ASEngine->RegisterInterface("Action");
+	ASEngine->RegisterInterfaceMethod("Action", "Action@ GetSuperAction()");
+	ASEngine->RegisterInterfaceMethod("Action", "Action@ AddSubAction(Action@ subAction)");
+	ASEngine->RegisterInterfaceMethod("Action", "Action@ If(Condition@ condition)");
+	ASEngine->RegisterInterfaceMethod("Action", "Action@ IfNot(Condition@ condition)");
+	ASEngine->RegisterInterfaceMethod("Action", "Action@ And(Action@ andAction)");
 	
-	ASEngine->RegisterInterface("ICondition");
-	ASEngine->RegisterInterfaceMethod("ICondition", "bool Check(Critter&, Critter@, Item@)");
+	ASEngine->RegisterInterfaceMethod("Action", "void SetSuperAction(Action@ superAction)");
+	ASEngine->RegisterInterfaceMethod("Action", "void CollectListeningActions(array<Action@>& collection)");
+	ASEngine->RegisterInterfaceMethod("Action", "void StartNextAction(Critter& critter)");
+	ASEngine->RegisterInterfaceMethod("Action", "bool Check(Critter& critter, Critter@ someCritter, Item@ someItem)");
+	ASEngine->RegisterInterfaceMethod("Action", "bool Start(Critter& critter)");
+	ASEngine->RegisterInterfaceMethod("Action", "void Cancel(Critter& critter)");
+	ASEngine->RegisterInterfaceMethod("Action", "string GetInfo()");
+	ASEngine->RegisterInterfaceMethod("Action", "void IdleEvent(Critter& critter)");
+	ASEngine->RegisterInterfaceMethod("Action", "void ShowCritterEvent(Critter& critter, Critter& showCritter)");
+	ASEngine->RegisterInterfaceMethod("Action", "void HideCritterEvent(Critter& critter, Critter& hideCritter)");
+	ASEngine->RegisterInterfaceMethod("Action", "bool AttackEvent(Critter& critter, Critter& target)");
+	ASEngine->RegisterInterfaceMethod("Action", "bool AttackedEvent(Critter& critter, Critter& attacker)");
+	ASEngine->RegisterInterfaceMethod("Action", "void DeadEvent(Critter& critter, Critter@ killer)");
+	ASEngine->RegisterInterfaceMethod("Action", "void MessageEvent(Critter& critter, Critter& messenger, int message, int value)");
+	ASEngine->RegisterInterfaceMethod("Action", "void SeeSomeoneAttackEvent(Critter& critter, Critter& attacker, Critter& attacked)");
+	ASEngine->RegisterInterfaceMethod("Action", "void SeeSomeoneDeadEvent(Critter& critter, Critter& killed, Critter@ killer)");
+	ASEngine->RegisterInterfaceMethod("Action", "void SeeSomeoneAttackedEvent(Critter& critter, Critter& attacked, Critter& attacker)");
+	ASEngine->RegisterInterfaceMethod("Action", "void SeeSomeoneStealingEvent(Critter& critter, Critter& victim, Critter& thief, bool success, Item& item, uint count)");
+	ASEngine->RegisterInterfaceMethod("Action", "void SeeSomeoneUseSkillEvent(Critter& critter, Critter& skillCritter, int skill, Critter@ onCritter, Item@ onItem, Scenery@ onScenery)");
+	ASEngine->RegisterInterfaceMethod("Action", "int PlaneBeginEvent(Critter& critter, NpcPlane& plane, int reason, Critter@ someCr, Item@ someItem)");
+	ASEngine->RegisterInterfaceMethod("Action", "int PlaneRunEvent(Critter& critter, NpcPlane& plane, int reason, uint& result0, uint& result1, uint& result2)");
+	ASEngine->RegisterInterfaceMethod("Action", "int PlaneEndEvent(Critter& critter, NpcPlane& plane, int reason, Critter@ someCr, Item@ someItem)");
+	
+	ASEngine->RegisterGlobalFunction("void StartCritterAction(Critter&, Action&, bool)", asFUNCTION(Start), asCALL_CDECL);
 	
     // Test Memory Level 3 for loaded DLLs
     for( uint i = 0; i < 666; i++ )
@@ -40,139 +63,231 @@ class CritterActionManager
 		map<uint, IAction*> queuedActions;
 	
 	public:
-		void Start(Critter& critter, IAction* action)
+		void Start(Critter* critter, IAction* action, bool immediatelly)
 		{
-			queuedActions[critter.Id] = action;
+			if(immediatelly)
+			{
+				runningActions[critter->Id] = action;
+				action->Start(critter);
+			}
+			else
+			{
+				queuedActions[critter->Id] = action;
+			}
 		}
 		
-		void StartQueuedAction(Critter& critter)
+		void StartQueuedAction(Critter* critter)
 		{
-			IAction* action = queuedActions[critter.Id];
+			IAction* action = queuedActions[critter->Id];
 			if(action != NULL)
 			{
-				Log("Found non-null action");
-				Log("Starting");
+				queuedActions[critter->Id] = NULL;
+				runningActions[critter->Id] = action;
 				action->Start(critter);
-				Log("Setting NULL to queued actions");
-				queuedActions[critter.Id] = NULL;
-				Log("Setting action as runnig");
-				runningActions[critter.Id] = action;
 			}
-			Log("End of start queued action");
 		}
 		
-		ScriptAny& GetListeningActions(Critter& critter)
+		void Finish(Critter* critter)
 		{
-			ScriptAny& actions = ScriptAny::Create();
-			IAction* runningAction = runningActions[critter.Id];
+			IAction* action = queuedActions[critter->Id];
+			if(action != NULL)
+				queuedActions[critter->Id] = NULL;
+				
+			action = runningActions[critter->Id];
+			if(action != NULL)
+			{
+				action->Cancel(critter);
+				runningActions[critter->Id] = NULL;
+			}
+		}
+		
+		void Respawn(Critter* critter)
+		{
+			IAction* action = runningActions[critter->Id];
+			if(action != NULL)
+			{
+				action->Cancel(critter);
+				runningActions[critter->Id] = NULL;
+				queuedActions[critter->Id] = action;
+			}
+		}
+		
+		vector<IAction*> GetListeningActions(Critter* critter)
+		{
+			IAction* runningAction = runningActions[critter->Id];
 			
 			if(runningAction == NULL)
-				return actions;
+				return vector<IAction*>(0);
 			
-			Log("Calling collect of listening actions");
-			runningAction->CollectListeningActions(actions);
-			Log("End of calling collect of listening actions");
-			
-			return actions;
+			return runningAction->GetListeningActions();
 		}
 };
 
 map<uint, IAction*> actionMap;
 CritterActionManager manager;
 
-EXPORT void Start(Critter& critter, IAction& action)
+void Start(Critter* critter, asIScriptObject* actionObj, bool immediately)
 {
-	manager.Start(critter, &action);
-}
-
-EXPORT void Idle(Critter& critter)
-{
-	Log("Trying Idle action");
-	manager.StartQueuedAction(critter);
-
-	Log("Getting listening actions");
-	ScriptAny& listeningActions = manager.GetListeningActions(critter);
-	
-	Log("Found some listening actions");
-	//~ for(int i = 0; i != listeningActions.GetElementSize(); i++) {
-		//~ ((IAction*) listeningActions.At(i))->Idle(critter);
-	//~ }
-}
-
-EXPORT bool Attack(Critter& critter, Critter& target)
-{
-	ScriptAny& listeningActions = manager.GetListeningActions(critter);
+	if(actionObj == NULL)
+		return;
 		
-	//~ for(int i = 0; i != listeningActions.GetElementSize(); i++) {
-		//~ bool result = ((IAction*) listeningActions.At(i))->Attack(critter, target);
-		//~ if(result)
-			//~ return true;
-	//~ }
-	return false;
-}
-
-EXPORT bool Attacked(Critter& critter, Critter& attacker)
-{
-	return false;
-}
-
-EXPORT void Dead(Critter& critter, Critter* killer)
-{
-}
-
-EXPORT void Finish(Critter& critter)
-{
-}
-
-EXPORT int PlaneBegin(Critter& critter, NpcPlane& plane, int reason, Critter* someCr, Item* someItem)
-{
-	return PLANE_KEEP;
-}
-
-EXPORT int PlaneRun(Critter& critter, NpcPlane& plane, int reason, uint& result0, uint& result1, uint& result2)
-{
-	return PLANE_KEEP;
-}
-
-EXPORT int PlaneEnd(Critter& critter, NpcPlane& plane, int reason, Critter* someCr, Item* someItem)
-{
-	return PLANE_KEEP;
-}
-
-EXPORT void SeeSomeoneAttack(Critter& critter, Critter& attacker, Critter& attacked)
-{
+	IAction* action = new ScriptActionWrapper(actionObj);
 	
+	manager.Start(critter, action, immediately);
 }
 
-EXPORT void SeeSomeoneDead(Critter& critter, Critter& killed, Critter* killer)
+EXPORT void Idle(Critter* critter)
 {
+	manager.StartQueuedAction(critter);
+	vector<IAction*> listeningActions = manager.GetListeningActions(critter);
+	
+	for(int i = 0; i < listeningActions.size(); i++)
+		listeningActions[i]->IdleEvent(critter);
 }
 
-EXPORT void SeeSomeoneAttacked(Critter& critter, Critter& attacked, Critter& attacker)
+EXPORT bool Attack(Critter* critter, Critter* target)
 {
+	vector<IAction*> listeningActions = manager.GetListeningActions(critter);
+		
+	bool result = false;
+	for(int i = 0; i != listeningActions.size(); i++)
+		result |= listeningActions[i]->AttackEvent(critter, target);
+		
+	return result;
 }
 
-EXPORT void SeeSomeoneStealing(Critter& critter, Critter& victim, Critter& thief, bool success, Item& item, uint count)
+EXPORT bool Attacked(Critter* critter, Critter* attacker)
 {
+	vector<IAction*> listeningActions = manager.GetListeningActions(critter);
+	
+	bool result = false;
+	for(int i = 0; i != listeningActions.size(); i++)
+		result |= listeningActions[i]->AttackedEvent(critter, attacker);
+		
+	return result;
 }
 
-EXPORT void ShowCritter(Critter& critter, Critter& showCritter)
+EXPORT void Dead(Critter* critter, Critter* killer)
 {
+	vector<IAction*> listeningActions = manager.GetListeningActions(critter);
+	
+	for(int i = 0; i != listeningActions.size(); i++)
+		listeningActions[i]->DeadEvent(critter, killer);
 }
 
-EXPORT void HideCritter(Critter& critter, Critter& hideCritter)
+EXPORT void Finish(Critter* critter, bool deleted)
 {
+	if(!deleted)
+		return;
+			
+	manager.Finish(critter);
 }
 
-EXPORT void Message(Critter& critter, Critter& messenger, int message, int value)
+EXPORT int PlaneBegin(Critter* critter, NpcPlane* plane, int reason, Critter* someCr, Item* someItem)
 {
+	vector<IAction*> listeningActions = manager.GetListeningActions(critter);
+		
+	int result = PLANE_RUN_GLOBAL;
+	for(int i = 0; i != listeningActions.size(); i++) {
+		int actionResult = listeningActions[i]->PlaneBeginEvent(critter, plane, reason, someCr, someItem);
+		if(actionResult != PLANE_RUN_GLOBAL)
+			result = actionResult;
+	}
+	return result;
 }
 
-EXPORT void Respawn(Critter& critter)
+EXPORT int PlaneRun(Critter* critter, NpcPlane* plane, int reason, uint& result0, uint& result1, uint& result2)
 {
+	vector<IAction*> listeningActions = manager.GetListeningActions(critter);
+		
+	int result = PLANE_RUN_GLOBAL;
+	for(int i = 0; i != listeningActions.size(); i++) {
+		int actionResult = listeningActions[i]->PlaneRunEvent(critter, plane, reason, result0, result1, result2);
+		if(actionResult != PLANE_RUN_GLOBAL)
+			result = actionResult;
+	}
+	return result;
 }
 
-EXPORT void SeeSomeoneUseSkill(Critter& critter, Critter& skillCritter, int skill, Critter* onCritter, Item* onItem, MapObject* onScenery)
+EXPORT int PlaneEnd(Critter* critter, NpcPlane* plane, int reason, Critter* someCr, Item* someItem)
 {
+	vector<IAction*> listeningActions = manager.GetListeningActions(critter);
+		
+	int result = PLANE_RUN_GLOBAL;
+	for(int i = 0; i != listeningActions.size(); i++) {
+		int actionResult = listeningActions[i]->PlaneEndEvent(critter, plane, reason, someCr, someItem);
+		if(actionResult != PLANE_RUN_GLOBAL)
+			result = actionResult;
+	}
+	return result;
+}
+
+EXPORT void SeeSomeoneAttack(Critter* critter, Critter* attacker, Critter* attacked)
+{
+	vector<IAction*> listeningActions = manager.GetListeningActions(critter);
+	
+	for(int i = 0; i != listeningActions.size(); i++)
+		listeningActions[i]->SeeSomeoneAttackEvent(critter, attacker, attacked);
+}
+
+EXPORT void SeeSomeoneDead(Critter* critter, Critter* killed, Critter* killer)
+{
+	vector<IAction*> listeningActions = manager.GetListeningActions(critter);
+	
+	for(int i = 0; i != listeningActions.size(); i++)
+		listeningActions[i]->SeeSomeoneDeadEvent(critter, killed, killer);
+}
+
+EXPORT void SeeSomeoneAttacked(Critter* critter, Critter* attacked, Critter* attacker)
+{
+	vector<IAction*> listeningActions = manager.GetListeningActions(critter);
+	
+	for(int i = 0; i != listeningActions.size(); i++)
+		listeningActions[i]->SeeSomeoneAttackedEvent(critter, attacked, attacker);
+}
+
+EXPORT void SeeSomeoneStealing(Critter* critter, Critter* victim, Critter* thief, bool success, Item* item, uint count)
+{
+	vector<IAction*> listeningActions = manager.GetListeningActions(critter);
+	
+	for(int i = 0; i != listeningActions.size(); i++)
+		listeningActions[i]->SeeSomeoneStealingEvent(critter, victim, thief, success, item, count);
+}
+
+EXPORT void ShowCritter(Critter* critter, Critter* showCritter)
+{
+	vector<IAction*> listeningActions = manager.GetListeningActions(critter);
+	
+	for(int i = 0; i != listeningActions.size(); i++)
+		listeningActions[i]->ShowCritterEvent(critter, showCritter);
+}
+
+EXPORT void HideCritter(Critter* critter, Critter* hideCritter)
+{
+	vector<IAction*> listeningActions = manager.GetListeningActions(critter);
+	
+	for(int i = 0; i != listeningActions.size(); i++)
+		listeningActions[i]->HideCritterEvent(critter, hideCritter);
+}
+
+EXPORT void Message(Critter* critter, Critter* messenger, int message, int value)
+{
+	vector<IAction*> listeningActions = manager.GetListeningActions(critter);
+	
+	for(int i = 0; i != listeningActions.size(); i++)
+		listeningActions[i]->MessageEvent(critter, messenger, message, value);
+}
+
+EXPORT void Respawn(Critter* critter)
+{
+	manager.Respawn(critter);
+}
+
+EXPORT void SeeSomeoneUseSkill(Critter* critter, Critter* skillCritter, int skill, Critter* onCritter, Item* onItem, MapObject* onScenery)
+{
+	vector<IAction*> listeningActions = manager.GetListeningActions(critter);
+	
+	for(int i = 0; i != listeningActions.size(); i++)
+		listeningActions[i]->SeeSomeoneUseSkillEvent(critter, skillCritter, skill, onCritter, onItem, onScenery);
 }
 
--- Server/scripts/cam.h
+++ Server/scripts/cam.h
@@ -6,23 +6,369 @@
 class IAction
 {
 	public:
-		virtual IAction* GetSuperAction();
-		virtual void SetSuperAction(IAction* superAction);
-		virtual void CollectListeningActions(ScriptAny& collection);
-		virtual void StartNextAction(Critter& critter);
-		virtual bool Check(Critter& critter, Critter* someCritter, Item* someItem);
-		virtual bool Start(Critter& critter);
-		virtual void Cancel(Critter& critter);
-		virtual string GetInfo();
+		IAction(){}
+		~IAction(){}
+	
+		virtual bool Start(Critter* critter) = 0;
+		virtual void Cancel(Critter* critter) = 0;
+		virtual vector<IAction*> GetListeningActions() = 0;
+		virtual const char* GetInfo() = 0;
 		
-		virtual void Idle(Critter& critter);
-		virtual bool Attack(Critter& critter, Critter& target);
+		virtual void IdleEvent(Critter* critter) = 0;
+		virtual void ShowCritterEvent(Critter* critter, Critter* showCritter) = 0;
+		virtual void HideCritterEvent(Critter* critter, Critter* hideCritter) = 0;
+		virtual bool AttackEvent(Critter* critter, Critter* target) = 0;
+		virtual bool AttackedEvent(Critter* critter, Critter* attacker) = 0;
+		virtual void DeadEvent(Critter* critter, Critter* killer) = 0;
+		virtual void MessageEvent(Critter* critter, Critter* messenger, int message, int value) = 0;
+		virtual void SeeSomeoneAttackEvent(Critter* critter, Critter* attacker, Critter* attacked) = 0;
+		virtual void SeeSomeoneDeadEvent(Critter* critter, Critter* killed, Critter* killer) = 0;
+		virtual void SeeSomeoneAttackedEvent(Critter* critter, Critter* attacked, Critter* attacker) = 0;
+		virtual void SeeSomeoneStealingEvent(Critter* critter, Critter* victim, Critter* thief, bool success, Item* item, uint count) = 0;
+		virtual void SeeSomeoneUseSkillEvent(Critter* critter, Critter* skillCritter, int skill, Critter* onCritter, Item* onItem, MapObject* onScenery) = 0;
+		virtual int PlaneBeginEvent(Critter* critter, NpcPlane* plane, int reason, Critter* someCr, Item* someItem) = 0;
+		virtual int PlaneRunEvent(Critter* critter, NpcPlane* plane, int reason, uint& result0, uint& result1, uint& result2) = 0;
+		virtual int PlaneEndEvent(Critter* critter, NpcPlane* plane, int reason, Critter* someCr, Item* someItem) = 0;
 };
 
-class ICondition
+class ScriptActionWrapper : public IAction
 {
+	private:
+		asIScriptContext* PrepareContext(char* methodName)
+		{
+			asIScriptContext* ctx = ASEngine->CreateContext();
+			asIScriptFunction* func = scriptAction->GetObjectType()->GetMethodByName(methodName);
+			
+			if(func == NULL)
+			{
+				Log("Cannot find function '%s'\n", methodName);
+				return NULL;
+			}
+			ctx->Prepare(func);
+			ctx->SetObject(scriptAction);
+			
+			return ctx;
+		}
+		
+		bool Execute(asIScriptContext* context)
+		{
+			int state = context->Execute();
+
+			if(state == asEXECUTION_EXCEPTION)
+			{
+				asIScriptFunction* ex = context->GetExceptionFunction();
+				Log("Exception occured: %s\n", context->GetExceptionString());
+				if(ex != NULL)
+					Log("Function exception: %s@%s\n", ex->GetModuleName(), ex->GetName());
+				Log("Line of exception: %d\n", context->GetExceptionLineNumber());
+				
+				return false;
+			}
+			
+			return true;
+		}
+	
+	protected:
+		asIScriptObject* scriptAction;
+		
 	public:
-		virtual bool Check(Critter& critter, Critter someCritter, Item someItem);
+		ScriptActionWrapper(asIScriptObject* scriptActionObj) : scriptAction(scriptActionObj) { scriptAction->AddRef(); }
+		~ScriptActionWrapper() { scriptAction->Release(); }
+	
+		asIScriptObject* GetScriptAction() { return scriptAction; }
+		
+		const char* GetInfo()
+		{
+			asIScriptContext* ctx = PrepareContext("GetInfo");
+			
+			if(ctx == NULL)
+				return "error";
+				
+			Execute(ctx);
+			ScriptString* result = (ScriptString*) ctx->GetReturnObject();
+			ctx->Release();
+			
+			return result->c_str();
+		}
+		
+		vector<IAction*> GetListeningActions()
+		{
+			asIScriptContext* ctx = PrepareContext("CollectListeningActions");
+			if(ctx == NULL)
+				return vector<IAction*>(0);
+			
+			ScriptArray& actionArray = ScriptArray::Create("Action@");
+			
+			//~ actionArray.AddRef();
+			
+			ctx->SetArgAddress(0, &actionArray);
+			Execute(ctx);
+
+			//~ Log("Collected %d listening actions from AS\n", actionArray.GetSize());
+			
+			vector<IAction*> actions(actionArray.GetSize());
+			for(int i = 0; i < actionArray.GetSize(); i++) 
+			{
+				asIScriptObject** obj = static_cast<asIScriptObject**>(actionArray.At(i));
+				//~ Log("Found listening action: %s\n", (*obj)->GetObjectType()->GetName());
+				ScriptActionWrapper* wrapper = new ScriptActionWrapper(*obj);
+				//~ Log("GetInfo of listening action: %s\n", wrapper->GetInfo());
+				actions[i] = wrapper;
+			}
+			//~ actionArray.Release();
+			ctx->Release();
+			
+			return actions;
+		}
+		
+		bool Start(Critter* critter)
+		{
+			asIScriptContext* ctx = PrepareContext("Start");
+			
+			if(ctx == NULL)
+				return false;
+				
+			ctx->SetArgObject(0, critter);
+			Execute(ctx);
+			
+			bool result = ctx->GetReturnByte();
+			ctx->Release();
+			return result;
+		}
+		
+		void Cancel(Critter* critter)
+		{
+			asIScriptContext* ctx = PrepareContext("Cancel");
+			
+			if(ctx == NULL)
+				return;
+				
+			ctx->SetArgObject(0, critter);
+			Execute(ctx);
+			ctx->Release();
+		} 
+		
+		void IdleEvent(Critter* critter)
+		{
+			asIScriptContext* ctx = PrepareContext("IdleEvent");
+			if(ctx == NULL)
+				return;
+				
+			ctx->SetArgObject(0, critter);
+			Execute(ctx);
+			ctx->Release();
+		}
+		
+		void ShowCritterEvent(Critter* critter, Critter* showCritter)
+		{
+			asIScriptContext* ctx = PrepareContext("ShowCritterEvent");
+			if(ctx == NULL)
+				return;
+				
+			ctx->SetArgObject(0, critter);
+			ctx->SetArgObject(1, showCritter);
+			Execute(ctx);
+			ctx->Release();
+		}
+		
+		void HideCritterEvent(Critter* critter, Critter* hideCritter)
+		{
+			asIScriptContext* ctx = PrepareContext("HideCritterEvent");
+			if(ctx == NULL)
+				return;
+				
+			ctx->SetArgObject(0, critter);
+			ctx->SetArgObject(1, hideCritter);
+			Execute(ctx);
+			ctx->Release();
+		}
+		
+		bool AttackEvent(Critter* critter, Critter* target)
+		{
+			asIScriptContext* ctx = PrepareContext("AttackEvent");
+			
+			if(ctx == NULL)
+				return false;
+				
+			ctx->SetArgObject(0, critter);
+			ctx->SetArgObject(1, target);
+			Execute(ctx);
+			
+			bool result = ctx->GetReturnByte();
+			ctx->Release();
+			return result;
+		}
+		
+		bool AttackedEvent(Critter* critter, Critter* attacker)
+		{
+			asIScriptContext* ctx = PrepareContext("AttackedEvent");
+			
+			if(ctx == NULL)
+				return false;
+				
+			ctx->SetArgObject(0, critter);
+			ctx->SetArgObject(1, attacker);
+			Execute(ctx);
+			
+			bool result = ctx->GetReturnByte();
+			ctx->Release();
+			return result;
+		}
+		
+		void DeadEvent(Critter* critter, Critter* killer)
+		{
+			asIScriptContext* ctx = PrepareContext("DeadEvent");
+			if(ctx == NULL)
+				return;
+				
+			ctx->SetArgObject(0, critter);
+			ctx->SetArgObject(1, killer);
+			Execute(ctx);
+			ctx->Release();
+		}
+		
+		void MessageEvent(Critter* critter, Critter* messenger, int message, int value)
+		{
+			asIScriptContext* ctx = PrepareContext("MessageEvent");
+			if(ctx == NULL)
+				return;
+				
+			ctx->SetArgObject(0, critter);
+			ctx->SetArgObject(1, messenger);
+			ctx->SetArgDWord(2, message);
+			ctx->SetArgDWord(3, value);
+			Execute(ctx);
+			ctx->Release();
+		}
+		
+		void SeeSomeoneAttackEvent(Critter* critter, Critter* attacker, Critter* attacked)
+		{
+			asIScriptContext* ctx = PrepareContext("SeeSomeoneAttackEvent");
+			if(ctx == NULL)
+				return;
+				
+			ctx->SetArgObject(0, critter);
+			ctx->SetArgObject(1, attacker);
+			ctx->SetArgObject(2, attacked);
+			Execute(ctx);
+			ctx->Release();
+		}
+		
+		void SeeSomeoneDeadEvent(Critter* critter, Critter* killed, Critter* killer)
+		{
+			asIScriptContext* ctx = PrepareContext("SeeSomeoneDeadEvent");
+			if(ctx == NULL)
+				return;
+				
+			ctx->SetArgObject(0, critter);
+			ctx->SetArgObject(1, killed);
+			ctx->SetArgObject(2, killer);
+			Execute(ctx);
+			ctx->Release();
+		}
+		
+		void SeeSomeoneAttackedEvent(Critter* critter, Critter* attacked, Critter* attacker)
+		{
+			asIScriptContext* ctx = PrepareContext("SeeSomeoneAttackedEvent");
+			if(ctx == NULL)
+				return;
+				
+			ctx->SetArgObject(0, critter);
+			ctx->SetArgObject(1, attacked);
+			ctx->SetArgObject(2, attacker);
+			Execute(ctx);
+			ctx->Release();
+		}
+		
+		void SeeSomeoneStealingEvent(Critter* critter, Critter* victim, Critter* thief, bool success, Item* item, uint count)
+		{
+			asIScriptContext* ctx = PrepareContext("SeeSomeoneStealingEvent");
+			if(ctx == NULL)
+				return;
+				
+			ctx->SetArgObject(0, critter);
+			ctx->SetArgObject(1, victim);
+			ctx->SetArgObject(2, thief);
+			ctx->SetArgByte(3, success);
+			ctx->SetArgObject(4, item);
+			ctx->SetArgDWord(5, count);
+			Execute(ctx);
+			ctx->Release();
+		}
+		
+		void SeeSomeoneUseSkillEvent(Critter* critter, Critter* skillCritter, int skill, Critter* onCritter, Item* onItem, MapObject* onScenery)
+		{
+			asIScriptContext* ctx = PrepareContext("SeeSomeoneUseSkill");
+			if(ctx == NULL)
+				return;
+				
+			ctx->SetArgObject(0, critter);
+			ctx->SetArgObject(1, skillCritter);
+			ctx->SetArgDWord(2, skill);
+			ctx->SetArgObject(3, onCritter);
+			ctx->SetArgObject(4, onItem);
+			ctx->SetArgObject(5, onScenery);
+			Execute(ctx);
+			ctx->Release();
+		}
+		
+		int PlaneBeginEvent(Critter* critter, NpcPlane* plane, int reason, Critter* someCr, Item* someItem)
+		{
+			asIScriptContext* ctx = PrepareContext("PlaneBeginEvent");
+			
+			if(ctx == NULL)
+				return false;
+				
+			ctx->SetArgObject(0, critter);
+			ctx->SetArgObject(1, plane);
+			ctx->SetArgDWord(2, reason);
+			ctx->SetArgObject(3, someCr);
+			ctx->SetArgObject(4, someItem);
+			Execute(ctx);
+			
+			int result = ctx->GetReturnDWord();
+			ctx->Release();
+			return result;
+		}
+		
+		int PlaneRunEvent(Critter* critter, NpcPlane* plane, int reason, uint& result0, uint& result1, uint& result2)
+		{
+			asIScriptContext* ctx = PrepareContext("PlaneRunEvent");
+			
+			if(ctx == NULL)
+				return false;
+				
+			ctx->SetArgObject(0, critter);
+			ctx->SetArgObject(1, plane);
+			ctx->SetArgDWord(2, reason);
+			ctx->SetArgAddress(3, &result0);
+			ctx->SetArgAddress(4, &result1);
+			ctx->SetArgAddress(5, &result2);
+			Execute(ctx);
+			
+			int result = ctx->GetReturnDWord();
+			ctx->Release();
+			return result;
+		}
+		
+		int PlaneEndEvent(Critter* critter, NpcPlane* plane, int reason, Critter* someCr, Item* someItem)
+		{
+			asIScriptContext* ctx = PrepareContext("PlaneEndEvent");
+			
+			if(ctx == NULL)
+				return false;
+				
+			ctx->SetArgObject(0, critter);
+			ctx->SetArgObject(1, plane);
+			ctx->SetArgDWord(2, reason);
+			ctx->SetArgObject(3, someCr);
+			ctx->SetArgObject(4, someItem);
+			Execute(ctx);
+			
+			int result = ctx->GetReturnDWord();
+			ctx->Release();
+			return result;
+		}
 };
 
 #endif     // __CRITTER_ACTION_MANAGER__
