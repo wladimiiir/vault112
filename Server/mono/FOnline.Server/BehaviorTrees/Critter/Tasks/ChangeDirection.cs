4b36fa69f4cd241c264dbec5088a023f6a9a8725
wladimiiir <wladimiiir@gmail.com>
2013-07-01 16:14:23 +0200

    correct backup server non mono executable

---
+++ Server/mono/FOnline.Server/BehaviorTrees/Critter/Tasks/ChangeDirection.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FOnline
+{
+	public class ChangeDirection
+	{
+		public ChangeDirection ()
+		{
+		}
+	}
+}
+
---
+++ Server/mono/FOnline.Server/Roles/Roles.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FOnline
+{
+	public class Roles
+	{
+		public Roles ()
+		{
+		}
+	}
+}
+
