7276afea8094b0f0366bdf26223d699fa6fb4627
wladimiiir <wladimiiir@gmail.com>
2013-07-12 14:34:19 +0200

    BT: Random, Weakest choosers

--- Server/mono/FOnline.Server/BehaviorTrees/Critter/Chooser/Random.cs
+++ Server/mono/FOnline.Server/BehaviorTrees/Critter/Chooser/Random.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 
 namespace FOnline.BT
 {
-	public class Random : CritterFilter<CritterBlackboard>
+	public class Random : CritterChooser<CritterBlackboard>
 	{
-		public override IList<Critter> Filter (IList<Critter> entitites)
+		public override Critter Choose (IList<Critter> entitites)
 		{
 			if (entitites.Count == 0)
-				return entitites;
+				return null;
 
-
-
-			return new Critter[] { entitites[Global.Random(0, entitites.Count - 1)] };
+			return entitites [Global.Random (0, entitites.Count - 1)];
 		}
 	}
 }
--- Server/mono/FOnline.Server/BehaviorTrees/Critter/Chooser/Weakest.cs
+++ Server/mono/FOnline.Server/BehaviorTrees/Critter/Chooser/Weakest.cs
@@ -3,11 +3,25 @@ using FOnline.BT;
 
 namespace FOnline.BT
 {
-	public class WeakestChooser : CritterChooser<CritterBlackboard>
+	public class Weakest : CritterChooser<CritterBlackboard>
 	{
 		public override Critter Choose (System.Collections.Generic.IList<Critter> entitites)
 		{
+			Critter weakest = null;
+			foreach (var entity in entitites) {
+				weakest = GetWeaker (entity, weakest);
+			}
+			return weakest;
+		}
 
+		private Critter GetWeaker (Critter critter, Critter otherCritter)
+		{
+			if (critter == null) 
+				return otherCritter;
+			if (otherCritter == null)
+				return critter;
+
+			return critter.Stat [Stats.CurrentHP] < otherCritter.Stat [Stats.CurrentHP] ? critter : otherCritter;
 		}
 	}
 }
